0cb48649283008a17945141cff544469095ed994
jill64 <intents.turrets0h@icloud.com>
2024-03-18 07:43:20 +0900

    Update CI workflow and add tests for StarSystem class

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,29 @@
 import { expect, test } from 'vitest'
+import 'dotenv/config'
+import { StarSystem } from '../src/StarSystem'
 
-test('test', () => {
-  expect(1 + 1).toBe(2)
+test('test', async () => {
+  const ss = new StarSystem({
+    name: 'star-system',
+    accountId: process.env.ACCOUNT_ID!,
+    apiKey: process.env.API_KEY!,
+  })
+
+  const main = await ss.createBranch(`main-${crypto.randomUUID()}`)
+
+  console.log('main',main)
+
+  const list = await ss.listBranches()
+  
+  console.log('list', list)
+
+  expect(list.length).toBe(1)
+  
+  const schema = await main.get_schema()
+
+  console.log('schema', schema)
+
+  const res = await main.delete()
+  
+  console.log('res', res)
 })
