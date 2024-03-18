8799cfc299150a8bf9bc91bfe6f8a8f1716cceb2
jill64 <intents.turrets0h@icloud.com>
2024-03-18 17:39:23 +0900

    Add support for adding tables with columns and indexes

--- src/types/TableDiff.ts
+++ src/types/TableDiff.ts
@@ -1,9 +1,15 @@
+import { Column } from './Column.js'
 import { ColumnDiff } from './ColumnDiff.js'
 import { IndexDiff } from './IndexDiff.js'
+import { IndexKey } from './IndexKey.js'
 import { Table } from './Table.js'
 
 export type TableDiff = {
-  addTables: Table[]
+  addTables: {
+    table: Table
+    columns: Column[]
+    indexes: IndexKey[]
+  }[]
   dropTables: Table[]
   modifyTables: (Table & {
     columnDiff: ColumnDiff
