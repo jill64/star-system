afe894c6e76c4af2db2883d1b00b5dc7967646e2
jill64 <intents.turrets0h@icloud.com>
2024-03-18 20:11:30 +0900

    Refactor Branch class to handle table and index differences

--- src/Branch.ts
+++ src/Branch.ts
@@ -1,5 +1,7 @@
 import type { D1 } from './D1.js'
 import { Column } from './types/Column.js'
+import { ColumnDiff } from './types/ColumnDiff.js'
+import { IndexDiff } from './types/IndexDiff.js'
 import { IndexKey } from './types/IndexKey.js'
 import { Table } from './types/Table.js'
 import { TableDiff } from './types/TableDiff.js'
@@ -38,15 +40,36 @@ export class Branch {
       tables.map(async (table) => {
         const [table_info, index_list] = await Promise.all([
           this.query<Column>(`PRAGMA table_info(${table.name})`),
-          this.query<IndexKey>(`PRAGMA index_list(${table.name})`)
+          this.query<Omit<IndexKey, 'columns'>>(
+            `PRAGMA index_list(${table.name})`
+          )
         ])
+
+        const indexes = await Promise.all(
+          index_list.result.map(({ results }) =>
+            Promise.all(
+              results.map(async (index) => {
+                const columns = await this.query<IndexKey['columns'][number]>(
+                  `PRAGMA index_info(${index.name})`
+                )
+
+                return {
+                  ...index,
+                  columns: columns.result.flatMap((column) => column.results)
+                }
+              })
+            )
+          )
+        )
 
         return [
           table.name,
           {
-            table,
-            columns: table_info.result.flatMap((column) => column.results),
-            indexes: index_list.result.flatMap((index) => index.results)
+            table: table satisfies Table,
+            columns: table_info.result.flatMap(
+              (column) => column.results
+            ) satisfies Column[],
+            indexes: indexes.flat() satisfies IndexKey[]
           }
         ] as const
       })
@@ -61,12 +84,12 @@ export class Branch {
       base.get_schema()
     ])
 
-    const addTables = [...head_schema.keys()]
+    const addTables: TableDiff['addTables'] = [...head_schema.keys()]
       .filter((table) => !base_schema.has(table))
       .map((table) => head_schema.get(table))
       .filter(nonNullable)
 
-    const dropTables = [...base_schema.keys()]
+    const dropTables: TableDiff['dropTables'] = [...base_schema.keys()]
       .filter((table) => !head_schema.has(table))
       .map((table) => base_schema.get(table)?.table)
       .filter(nonNullable)
@@ -81,58 +104,26 @@ export class Branch {
           throw new Error('Unexpected undefined')
         }
 
-        const columnDiff = {
+        const columnDiff: ColumnDiff = {
           addColumns: head.columns.filter(
             (column) => !base.columns.some((c) => c.name === column.name)
-          ),
-          dropColumns: base.columns.filter(
-            (column) => !head.columns.some((c) => c.name === column.name)
-          ),
-          modifyColumns: head.columns.filter((column) => {
-            const baseColumn = base.columns.find((c) => c.name === column.name)
-
-            if (baseColumn === undefined) {
-              return false
-            }
-
-            return (
-              baseColumn.type !== column.type ||
-              baseColumn.notnull !== column.notnull ||
-              baseColumn.dflt_value !== column.dflt_value ||
-              baseColumn.pk !== column.pk ||
-              baseColumn.cid !== column.cid
-            )
-          })
+          )
         }
 
-        const indexDiff = {
+        const indexDiff: IndexDiff = {
           addIndexes: head.indexes.filter(
             (index) => !base.indexes.some((i) => i.name === index.name)
           ),
           dropIndexes: base.indexes.filter(
             (index) => !head.indexes.some((i) => i.name === index.name)
-          ),
-          modifyIndexes: head.indexes.filter((index) => {
-            const baseIndex = base.indexes.find((i) => i.name === index.name)
-
-            if (baseIndex === undefined) {
-              return false
-            }
-
-            return (
-              baseIndex.unique !== index.unique ||
-              baseIndex.origin !== index.origin ||
-              baseIndex.partial !== index.partial ||
-              baseIndex.seq !== index.seq
-            )
-          })
+          )
         }
 
         return {
           ...head.table,
           columnDiff,
           indexDiff
-        }
+        } satisfies TableDiff['modifyTables'][number]
       })
 
     return {
@@ -142,7 +133,80 @@ export class Branch {
     }
   }
 
-  async deploy(diff: TableDiff) {
-    
+  async mergeTo(
+    base: Branch,
+    callback: (
+      status:
+        | 'DROPPING_TABLE'
+        | 'DROPPING_INDEX'
+        | 'CREATING_TABLE'
+        | 'CREATING_COLUMN'
+        | 'CREATING_INDEX'
+    ) => unknown
+  ) {
+    const diff = await this.diffFrom(base)
+
+    await callback('DROPPING_TABLE')
+
+    await Promise.all(
+      diff.dropTables.map((table) => base.query(`DROP TABLE ${table.name}`))
+    )
+
+    await callback('DROPPING_INDEX')
+
+    await Promise.all(
+      diff.modifyTables.flatMap((table) =>
+        table.indexDiff.dropIndexes.map((index) =>
+          base.query(`DROP INDEX ${index.name}`)
+        )
+      )
+    )
+
+    await callback('CREATING_TABLE')
+
+    await Promise.all(
+      diff.addTables.map((table) => {
+        const columns = table.columns
+          .map(
+            (column) =>
+              `${column.name} ${column.type}${
+                column.notnull ? ' NOT NULL' : ''
+              }${column.dflt_value ? ` DEFAULT ${column.dflt_value}` : ''}`
+          )
+          .join(', ')
+
+        return base.query(`CREATE TABLE ${table.table.name} (${columns})`)
+      })
+    )
+
+    await callback('CREATING_COLUMN')
+
+    await Promise.all(
+      diff.modifyTables.flatMap((table) =>
+        table.columnDiff.addColumns.map((column) =>
+          base.query(
+            `ALTER TABLE ${table.name} ADD COLUMN ${column.name} ${column.type}${
+              column.notnull ? ' NOT NULL' : ''
+            }${column.dflt_value ? ` DEFAULT ${column.dflt_value}` : ''}`
+          )
+        )
+      )
+    )
+
+    await callback('CREATING_INDEX')
+
+    await Promise.all(
+      diff.modifyTables.flatMap((table) =>
+        table.indexDiff.addIndexes.map((index) => {
+          const columns = index.columns
+            .map((column) => column.name)
+            .join(', ')
+
+          return base.query(
+            `CREATE INDEX ${index.name} ON ${table.name} (${columns})`
+          )
+        })
+      )
+    )
   }
 }
